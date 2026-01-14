35b53cb91ec4571f5f9862621c92ed97f84e6b70
Safeya-Yasien <ssafeya5@gmail.com>
2026-01-14 14:02:16 +0200

    Add user data retrieval and display functionality
    - Created index.html to serve as the main entry point for the application.
    - Implemented index.js to fetch user data from the API and display user cards with first name, last name, email, address, and phone number.
    - Added style.css for basic styling of the user cards and layout.
    - Created user.html for displaying individual user details based on user ID input.
    - Implemented user.js to handle user ID input and fetch specific user data from the API.

---
+++ js/day_8_lab1/index.js
@@ -0,0 +1,43 @@
+// server 500
+// client 400
+
+// 1-	Use AJAX to retrieve users data, and display: First Name, Last name and address and phone from the following test web API: https://jsonplaceholder.typicode.com/users
+
+const apiUrl = "https://dummyjson.com/users";
+var request = new XMLHttpRequest();
+request.open("GET", apiUrl);
+request.send();
+
+request.onload = () => {
+  if (request.status === 200) {
+    // console.log("response before parsing", request.response);
+    // Use JSON.parse() to convert the string into a JavaScript object
+    const response = JSON.parse(request.response);
+    // console.log("response after parsing", response);
+    const data = response.users;
+    console.log("response after parsing", data);
+    const container = document.querySelector(".container");
+    container.innerHTML = "";
+
+    for (let i = 0; i < data.length; i++) {
+      const user = data[i];
+      const userCard = document.createElement("div");
+      userCard.classList.add("user-card");
+      userCard.innerHTML = `
+        <div class="user-card-header">
+          <h2>${user.firstName} ${user.lastName}</h2>
+          <p>${user.company.name}</p>
+        </div>
+        <div class="user-card-body">
+          <p>${user.email}</p>
+          <p>${user.address.address}</p>
+          <p>${user.phone}</p>
+        </div>`;
+
+      container.appendChild(userCard);
+    }
+  } else {
+    // Handle error
+    console.log("Error:", request.status, request.statusText);
+  }
+};
