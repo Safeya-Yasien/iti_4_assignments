df0490359ca7107b23739cf3f7e014cb72e42018
Safeya-Yasien <ssafeya5@gmail.com>
2026-01-18 20:26:10 +0200

    Add initial HTML structure and reservation functionality
    - Created index.html with basic structure and styles.
    - Added reservation.html with a table for displaying reserved books and a delete button.
    - Implemented reservation.js to handle fetching and displaying reserved book data from localStorage and a JSON file.
    - Included functionality to update and delete reserved books.

---
+++ advanced_js/day_1/reservation.js
@@ -0,0 +1,87 @@
+const reservation = document.querySelector("#reservation");
+const deleteAll = document.querySelector("#delete-all");
+
+function getReservation() {
+  const reservedBooks = JSON.parse(localStorage.getItem("reservedBooks")) || [];
+
+  for (book of reservedBooks) {
+    let bookId = book.id;
+    let quantity = book.Quantity;
+    getBookData(bookId, quantity);
+  }
+}
+
+function getBookData(bookId, quantity) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://127.0.0.1:5500/advanced_js/day_1/BookList.json");
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      const data = JSON.parse(xhr.responseText);
+
+      for (book of data) {
+        if (book.id == Number(bookId)) {
+          displayData(book, quantity);
+        }
+      }
+    }
+  };
+  xhr.send();
+}
+
+function displayData(book, quantity) {
+  let tr = document.createElement("tr");
+  tr.innerHTML = `
+                <td>${book.title}</td>
+                <td>${book.author}</td>
+                <td>${book.category}</td>
+                <td>
+                    <input type='number' value='${quantity}' onChange="changeValue(this,${book.id})"/>
+                </td>
+                    `;
+  reservation.appendChild(tr);
+}
+
+function changeValue(element, bookId) {
+  const reservedBooks = JSON.parse(localStorage.getItem("reservedBooks")) || [];
+
+  if (element.value != 0) {
+    for (book of reservedBooks) {
+      if (book.id == bookId) {
+        book.Quantity = parseInt(element.value);
+        break;
+      }
+    }
+  } else {
+    const confirmMessage = confirm(
+      "Are you sure you want to delete this book?",
+    );
+    if (confirmMessage) {
+      let index = -1;
+      for (let i = 0; i < reservedBooks.length; i++) {
+        if (reservedBooks[i].id == bookId) {
+          index = i;
+          break;
+        }
+      }
+
+      if (index !== -1) {
+        reservedBooks.splice(index, 1);
+
+        element.closest("tr").remove();
+      }
+    } else {
+      element.value = 1;
+    }
+  }
+  localStorage.setItem("reservedBooks", JSON.stringify(reservedBooks));
+}
+
+deleteAll.addEventListener("click", function () {
+  const confirmMessage = confirm("Are you sure you want to delete all books?");
+  if (confirmMessage) {
+    localStorage.removeItem("reservedBooks");
+    location.reload();
+  }
+});
+
+getReservation();
