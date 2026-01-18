0963ff9e2c28c35dbb8cb7ea2c87ade7061bc662
Safeya-Yasien <ssafeya5@gmail.com>
2026-01-18 13:46:05 +0200

    update login.js

--- advanced_js/Day 1 - Ajax, Cookies, Web Storage/login.js
+++ advanced_js/Day 1 - Ajax, Cookies, Web Storage/login.js
@@ -1,3 +1,6 @@
+// when reload page email should be filled auto from cookies
+// but if iw want the email which user logged it immediately then also the email from cookie it will appears
+
 const form = document.querySelector("form");
 const email = document.querySelector("#email");
 const card_number = document.querySelector("#card_number");
@@ -6,6 +9,8 @@ const remember_me = document.querySelector("#remember_me");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  getEmailFromCookie();
 
   const isValidCardNumber = checkCardNumber(card_number.value);
   const isValidEmail = checkEmail(email.value);
@@ -19,12 +24,21 @@ form.addEventListener("submit", (e) => {
       let date = new Date();
       date.setDate(date.getDate() + 1);
       document.cookie = `email=${email.value}; expires=${date}`;
-      location.href = "reservation.html";
+      location.href = "books.html";
     }
   } else {
     error.innerHTML = "Invalid Card Number or Email";
   }
 });
+
+function getEmailFromCookie() {
+  let storageEmail = document.cookie.split("=")[1];
+  //   console.log(email);
+
+  if (storageEmail != null) {
+    email.value = storageEmail;
+  }
+}
 
 function checkCardNumber(cardNumber) {
   // card code must starts with LIB
