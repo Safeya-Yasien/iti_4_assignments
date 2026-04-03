00b96721381d9d5bdcbaa0ec3e466b011a36ece1
Safeya-Yasien <ssafeya5@gmail.com>
2026-04-03 19:04:41 +0200

    update layout.tsx

--- nextjs/day1/my-app/app/(profile)/layout.tsx
+++ nextjs/day1/my-app/app/(profile)/layout.tsx
@@ -2,14 +2,21 @@ import Link from "next/link";
 
 const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex ">
-      <aside>Sidebar</aside>
-      <div>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/info">Info</Link>
+    <div className="flex  ">
+      <div className="w-50">
+        <aside className="mb-4 text-blue-600">Sidebar</aside>
+        <div className="flex flex-col  gap-4 mb-8">
+          <Link href="/" className="hover:text-red-500">
+            Home
+          </Link>
+          <Link href="/about" className="hover:text-red-500">
+            About
+          </Link>
+          <Link href="/info" className="hover:text-red-500">
+            Info
+          </Link>
+        </div>
       </div>
-
       <div>{children}</div>
     </div>
   );
